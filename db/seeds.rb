0ffe36405f5667ad2883812eeb8158fd3d79ee15
Sandy Ludosky <sandyludosky@gmail.com>
2014-07-21 18:24:05 -0400

    added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+picture = %w(abstract city animals people transport food nature business nightlife sports fashion technics)
+
+10.times do |title, description, author|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = picture[(rand(0..12))].to_s + "/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url
+    )
+end
