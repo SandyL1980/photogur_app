2a8f18213b75ce82eb5797fb67b358f8742c8e06
Sandy Ludosky <sandyludosky@gmail.com>
2014-07-26 22:12:06 -0400

    generated resource for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
+
   root to: 'pictures#index'
   resources :pictures
+  resources :categories
 
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,219 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-picture = %w(abstract city animals people transport food nature business nightlife sports fashion technics)
 
-10.times do |title, description, author|
+category = %w(abstract city animals people transport food nature business nightlife sports fashion technics)
+
+
+category.each do |name|
+    Category.create(name: name)
+end
+
+abstract = Category.where(name: "abstract").first
+city = Category.where(name: "city").first
+animals = Category.where(name: "animals").first
+people = Category.where(name: "people").first
+transport = Category.where(name: "transport").first
+animals = Category.where(name: "animals").first
+food = Category.where(name: "food").first
+nature = Category.where(name: "nature").first
+business = Category.where(name: "business").first
+nightlife = Category.where(name: "nightlife").first
+fashion = Category.where(name: "fashion").first
+sports = Category.where(name: "sports").first
+technics = Category.where(name: "technics").first
+
+
+
+2.times do |title, description, author, category|
 
   title = Faker::Company.catch_phrase
   description = Faker::Lorem.paragraph
-  picture_url = picture[(rand(0..12))].to_s + "/" + rand(1..9).to_s + "/" 
+  picture_url = "/abstract/" + rand(1..9).to_s + "/" 
   artist = Faker::Name.name
 
   Picture.create!( 
     :title => title,
     :description => description,
     :artist => artist,
-    :picture_url => picture_url
+    :picture_url => picture_url,
+    :category_id => abstract.id
     )
 end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/people/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => people.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/city/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => city.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/transport/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => transport.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/animals/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => animals.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/food/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => food.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/nature/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => nature.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/business/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => business.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/nightlife/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => nightlife.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/sports/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => sports.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/fashion/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => fashion.id
+    )
+end
+
+2.times do |title, description, author, category|
+
+  title = Faker::Company.catch_phrase
+  description = Faker::Lorem.paragraph
+  picture_url = "/technics/" + rand(1..9).to_s + "/" 
+  artist = Faker::Name.name
+
+  Picture.create!( 
+    :title => title,
+    :description => description,
+    :artist => artist,
+    :picture_url => picture_url,
+    :category_id => technics.id
+    )
+end
+
