54c7efb5f585be94d4a78c572168d79bbe7473b8
Sandy Ludosky <sandyludosky@gmail.com>
2014-07-31 11:45:16 -0400

    modified name of users_controller file - was mispelled

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'pictures#index'
+  
+
   resources :reviews
 
-  root to: 'pictures#index'
-
+  resources :users
   devise_for :users
 
   resources :pictures do
@@ -13,7 +15,7 @@ Rails.application.routes.draw do
 	 end
 
   resources :categories
-  resources :users
+ 
 
   
 end
